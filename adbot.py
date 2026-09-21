7e61633ce4c112d8c59da12ce8c54eb38718fad0
aryanes90 <eskandarearyan8@gmail.com>
2026-09-21 15:15:36 +0330

    Update default usernames and environment variables

--- adbot.py
+++ adbot.py
@@ -23,8 +23,8 @@ logger = logging.getLogger(__name__)
 # تنظیمات پیش‌فرض
 CONFIG_FILE = "bot_config.json"
 DEFAULT_CONFIG = {
-    "host_usernames": ["ad0ri"],
-    "admin_usernames": ["ad0ri"],
+    "host_usernames": ["aryan_0990_"],
+    "admin_usernames": ["aryan_0990_"],
     "vip_usernames": [],
     "banned_users": [],
     "teleport_locations": {
@@ -2713,11 +2713,11 @@ async def main():
     from http.server import BaseHTTPRequestHandler, HTTPServer
     
     logger.info("تلاش برای بارگذاری متغیرهای محیطی...")
-    room_id = os.getenv("ROOM_ID", "68e771922d585712212e8070")
-    api_token = os.getenv("API_TOKEN", "ed578ca3bb6a6592059a878e4d807d0c7eac56742239fdcce7ca84841f2437e5")
+    room_id = os.getenv("ROOM_ID", "6946bffc94977936f7937068")
+    api_token = os.getenv("API_TOKEN", "6ab03bbeb4720b3a39b895f7")
     
     if not room_id or not api_token:
-        logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
+        logger.error("ROOM_ID  API_TOKEN  ")
         return
     
     logger.info(f"ROOM_ID: {room_id}")
