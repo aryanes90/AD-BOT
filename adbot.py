be384a54b29ea1664642c2d7bdb75db24a8780d8
aryanes90 <eskandarearyan8@gmail.com>
2026-09-21 15:27:29 +0330

    See
    Replaced sensitive information in logs with placeholders.

--- adbot.py
+++ adbot.py
@@ -2720,8 +2720,8 @@ async def main():
         logger.error("ROOM_ID یا API_TOKEN تنظیم نشده‌اند.")
         return
     
-    logger.info(f"ROOM_ID: {room_id}")
-    logger.info(f"API_TOKEN: {api_token}")
+    logger.info(f"ROOM_ID: {6946bffc94977936f7937068}")
+    logger.info(f"API_TOKEN: {6ab03bbeb4720b3a39b895f7}")
 
     # ساختار وب‌سرور داخلی و سبک پایتون
     class PingHandler(BaseHTTPRequestHandler):
